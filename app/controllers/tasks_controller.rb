ac59e8e7ae74bb83fea1eb0653ec0ed830e8d43c
atsuto.okada <oa2to0729@gmail.com>
2020-07-06 12:48:13 +0000

    route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
     before_action :require_user_logged_in
-    before_action :correct_user, only: [:edit, :show]
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :correct_user, only: [:edit, :show, :update, :destroy]
     
     def index
         @tasks = current_user.tasks.order(id: :desc)
